ffbaf3477c9b5204eac1a9494746baf9a14dc04d
shyam69 <shyamlohar1996@gmail.com>
2018-03-17 19:25:32 +0530

    added hover effects and added follow me section

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ $(window).scroll(function(){
     var rotateDeg = Math.min(90,top*0.7)
     $('.img1').css({'transform':`rotateZ(${rotateDeg}deg)`})
     
+    if (top > $('.profile').offset().top - $(window).height() / 3.2) {
+        console.log('kek')
+        $('.profile h4').css({ 'transform': 'translateY(0%)' })
+    }
+
+    if (top > $('.works').offset().top - $(window).height()) {
+        var offset = (Math.min(0, top - $('.works').offset().top + $(window).height() - 350)).toFixed();
+
+        $('.works a:nth-child(1)').css({ 'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+
+        $('.works a:nth-child(3)').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+    }
 })
 
 
